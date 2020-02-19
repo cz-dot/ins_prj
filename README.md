1985db9e84b0e7fa407f6abd4c211d33cace5d3e
Chang Zhou <zhouchang29@gmail.com>
2020-02-18 16:43:03 -0800

    Firebase

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+Now it can access on Google Firebase [insprj](https://insprj.web.app/).
+
 ## Available Scripts
 
 In the project directory, you can run:
---
+++ public/precache-manifest.5ab9e593b1a3d4a0fc36bfe0f80e7ede.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa697a7a510623d00edec5818a5984a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c62164aefca2dfbec73",
+    "url": "/static/css/2.109e678f.chunk.css"
+  },
+  {
+    "revision": "a50de3d24c83b77d4c82",
+    "url": "/static/css/main.e09109c2.chunk.css"
+  },
+  {
+    "revision": "8c62164aefca2dfbec73",
+    "url": "/static/js/2.4237d9da.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.4237d9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50de3d24c83b77d4c82",
+    "url": "/static/js/main.f6ff5822.chunk.js"
+  },
+  {
+    "revision": "4f4a138c45d99332aa9c",
+    "url": "/static/js/runtime-main.03d48c79.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
